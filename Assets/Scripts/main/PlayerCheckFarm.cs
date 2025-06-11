56dbff87f12e9f597a188a6794281a237ced4ff3
游淯丞 <102642636+znxdg@users.noreply.github.com>
2025-06-12 01:43:11 +0800

    [YC]更新農田栽種邏輯
    修正隔空種植BUG

--- Assets/Scripts/main/PlayerCheckFarm.cs
+++ Assets/Scripts/main/PlayerCheckFarm.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace YuCheng
 {
@@ -9,6 +10,8 @@ namespace YuCheng
         protected string farmTag;
         [HideInInspector]
         public GameObject farmObject;
+
+        private List<GameObject> currentFarms = new List<GameObject>();     // 存放同時觸碰到的農田
 
         private static PlayerCheckFarm _instance;
         public static PlayerCheckFarm instance
@@ -27,18 +30,29 @@ namespace YuCheng
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag(farmTag))
-                farmObject = collision.gameObject;
+            {
+                if (!currentFarms.Contains(collision.gameObject)) currentFarms.Add(collision.gameObject);
+                farmObject = currentFarms[currentFarms.Count - 1]; // 取最後一個進入的
+            }
         }
 
         /// <summary>
         /// 碰撞箱離開農田做清除
         /// </summary>
         /// <param name="collision"></param>
-        //private void OnTriggerExit2D(Collider2D collision)
-        //{
-        //    if (collision.CompareTag(farmTag))
-        //        farmObject = null;
-        //}
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.CompareTag(farmTag))
+            {
+                currentFarms.Remove(collision.gameObject);
+
+                // 若還有重疊中的農田，就改用最後一個
+                if (currentFarms.Count > 0)
+                    farmObject = currentFarms[currentFarms.Count - 1];
+                else
+                    farmObject = null;
+            }
+        }
 
         /// <summary>
         /// 是否站在農田旁
