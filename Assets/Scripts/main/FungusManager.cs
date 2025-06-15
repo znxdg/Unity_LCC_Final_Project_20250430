f2b8e4cc287428dc50b5967b00020879734cc253
游淯丞 <102642636+znxdg@users.noreply.github.com>
2025-06-15 15:33:09 +0800

    [YC]Fungus對話框優化

---
+++ Assets/Scripts/main/FungusManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace YuCheng
+{
+    /// <summary>
+    /// 用來記錄各個對話框狀態
+    /// </summary>
+    public class FungusManager : MonoBehaviour
+    {
+        public static FungusManager instance;
+
+        [Header("所有對話框顯示狀態")]
+        [SerializeField]
+        private List<Fungus_Item> dialogFlags = new List<Fungus_Item>();
+
+        void Awake()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/main/Fungus_Item.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace YuCheng
+{
+    [CreateAssetMenu(menuName = "YC/Fungus_State", order = 0)]
+    public class Fungus_Item : ScriptableObject
+    {
+        public string fungus_name;
+        public bool fungusShowState;
+    }
+}
