a79b6815ef1f70d7c27149adcb6bc21e2efc2f00
游淯丞 <102642636+znxdg@users.noreply.github.com>
2025-06-16 02:10:35 +0800

    [YC]完善對話框只顯示一次功能

---
+++ Assets/Scripts/main/CheckFlagThenContinue.cs
@@ -0,0 +1,40 @@
+﻿using Fungus;
+using UnityEngine;
+
+
+namespace YuCheng
+{
+    [CommandInfo("Custom", "Check Flag Then Continue", "判斷一個腳本物件的 bool 值，為 true 則繼續執行，否則跳過 Block")]
+    public class CheckFlagThenContinue : Command
+    {
+        [Tooltip("要判斷的腳本物件")]
+        public Fungus_Item dialogueFlag;
+
+        public Player player;
+
+        //[Tooltip("如果是 false，是否結束整個 block？")]
+        //public bool stopBlockIfFalse = true;
+
+        public override void OnEnter()
+        {
+            if (dialogueFlag == null)
+            {
+                Debug.LogWarning("未指定 DialogueFlagSO");
+                Continue();
+                return;
+            }
+
+            if (!dialogueFlag.fungusShowState)
+            {
+                // 繼續執行 Block
+                Continue();
+                dialogueFlag.fungusShowState = true;
+            }
+            else
+            {
+                StopParentBlock();
+                if (player != null) player.canMove = true;
+            }
+        }
+    }
+}
--- Assets/Scripts/main/Fungus_Item.cs
+++ Assets/Scripts/main/Fungus_Item.cs
@@ -5,7 +5,9 @@ namespace YuCheng
     [CreateAssetMenu(menuName = "YC/Fungus_State", order = 0)]
     public class Fungus_Item : ScriptableObject
     {
+        [Header("使用區塊名稱")]
         public string fungus_name;
+        [Header("對話是否已經執行過")]
         public bool fungusShowState;
     }
 }
