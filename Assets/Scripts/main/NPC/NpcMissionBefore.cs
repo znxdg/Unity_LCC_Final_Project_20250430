5db578ee660979df8572a8586e2e408f13ac8dda
游淯丞 <102642636+znxdg@users.noreply.github.com>
2025-06-23 01:21:59 +0800

    [YC] 完善劇情：與NPC互動

--- Assets/Scripts/main/NPC/NpcMissionBefore.cs
+++ Assets/Scripts/main/NPC/NpcMissionBefore.cs
@@ -7,8 +7,23 @@ namespace YuCheng
     /// </summary>
     public class NpcMissionBefore : NpcState
     {
+        private bool isInteraction;
+
         public NpcMissionBefore(NPC _npc, StateMachine _stateMachine, string _name) : base(_npc, _stateMachine, _name)
         {
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (isInteraction && timer > 1) stateMachine.SwitchState(npc.missioning); 
+        }
+
+        protected override void Interaction()
+        {
+            base.Interaction();
+            isInteraction = true;
+            npc.fungus.SendFungusMessage("任務前");
         }
     }
 }
--- Assets/Scripts/main/NPC/NpcState.cs
+++ Assets/Scripts/main/NPC/NpcState.cs
@@ -8,12 +8,37 @@ namespace YuCheng
     public class NpcState : State
     {
         protected NPC npc;
+        public bool playerInArea;
 
         public NpcState(NPC _npc, StateMachine _stateMachine, string _name)
         {
             npc = _npc;
             stateMachine = _stateMachine;
             name = _name;
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            playerInArea = npc.isEnter;
+
+            if (playerInArea && Input.GetKeyDown(KeyCode.E)) Interaction();
+        }
+
+        protected virtual void Interaction()
+        {
+            Print.Text("開始互動", "#f66");
         }
     }
 }
